fbd9eb54d87a5e222fed1a8c2ed466a4443efc25
Chris Glazier <chris@vantageinnovative.com>
2021-07-22 20:32:39 -0400

    restructured html, comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// this funciton immediately below is actually jQuery, jQuery makes working with the DOM a little bit easier than plain jane js but you are not bringing it in to your html page so javascript doesnt know how to read it
+// google jQuery cdn if you wanted to use it in this project.
+
 $(function() {
 	$('.toggler').on('click', function() {
 		$('nav').slideToggle(500);
@@ -7,6 +10,7 @@ $(function() {
 
 function submitFirstName(event)
 {
+    console.log(event)
 	event.preventDefault();
 	// on submit, test the name field//
 	 let firstName = document.getElementById ('name');
